be022ceeddee5e27764e74e0ce99a274ee04b6e0
GitBob <enzobobdevvideos04@gmail.com>
2026-09-02 23:43:22 -0300

    Update the build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.toolgits.xezenon"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     buildTypes {
